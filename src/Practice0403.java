4690911abe417ade3c5890dff086747d83fe924c
jwlee-9939-study <jwlee9939@gmail.com>
2022-05-14 13:20:46 +0900

    매출액의 종류(Hash, sliding window) 로직을 더욱 간단하게 수정

--- src/Practice0403.java
+++ src/Practice0403.java
@@ -4,18 +4,19 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Practice0403 {
-    public ArrayList<Integer> solution(int num, int[] arr) {
+    public ArrayList<Integer> solution(int k, int[] arr) {
         ArrayList<Integer> answer = new ArrayList<>();
-        int left = 0, right = num;
         HashMap<Integer, Integer> hMap = new HashMap<>();
-        while (right <= arr.length) {
-           for (int i = left; i < right; i++) {
-               hMap.put(arr[i], hMap.getOrDefault(arr[i], 0) + 1);
-           }
-            System.out.println(hMap);
-           answer.add(hMap.size());
-           hMap.remove(arr[left++]);
-           right++;
+        for (int i = 0; i < k - 1; i++) {
+            hMap.put(arr[i], hMap.getOrDefault(arr[i], 0) + 1);
+        }
+        int left = 0;
+        for (int right = k - 1; right < arr.length; right++) {
+            hMap.put(arr[right], hMap.getOrDefault(arr[right], 0) + 1);
+            answer.add(hMap.size());
+            hMap.put(arr[left], hMap.getOrDefault(arr[left], 0) - 1);
+            if (hMap.get(arr[left]) == 0) hMap.remove(arr[left]);
+            left++;
         }
         return answer;
     }
