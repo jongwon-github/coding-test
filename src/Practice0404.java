f37b142629e2216c7cbb8c4c2d648ea541d111c6
jwlee-9939-study <jwlee9939@gmail.com>
2022-05-17 06:32:00 +0900

    모든 아나그램 찾기(Hash, sliding window : 시간복잡도 O(n)) -> 완료

--- src/Practice0404.java
+++ src/Practice0404.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Practice0404 {
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Practice0404 {
+    public int solution(String a, String b) {
+        int answer = 0;
+        HashMap<Character, Integer> am = new HashMap<>();
+        HashMap<Character, Integer> bm = new HashMap<>();
+        for (int i = 0; i < b.length() - 1; i++) {
+            am.put(a.charAt(i), am.getOrDefault(a.charAt(i), 0) + 1);
+        }
+        for (char x : b.toCharArray()) bm.put(x, bm.getOrDefault(x, 0) + 1);
+        int lt = 0;
+        for (int rt = b.length() - 1; rt < a.length(); rt++) {
+            am.put(a.charAt(rt), am.getOrDefault(a.charAt(rt), 0) + 1);
+            if (am.equals(bm)) answer++;
+            am.put(a.charAt(lt), am.get(a.charAt(lt)) - 1);
+            if (am.get(a.charAt(lt)) == 0) am.remove(a.charAt(lt));
+            lt++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Practice0404 T = new Practice0404();
+        Scanner kb = new Scanner(System.in);
+        String a = kb.next();
+        String b = kb.next();
+        System.out.println(T.solution(a, b));
+    }
 }
