b4dd5b1daa8db76fc1e951fc9a7f860fd8f3412d
jwlee-9939-study <jwlee9939@gmail.com>
2022-05-23 06:22:59 +0900

    Stack, Queue(자료구조) - 괄호문자제거 완성

--- src/Practice0502.java
+++ src/Practice0502.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Practice0502 {
+    /*
+    * stack의 pop은 제거와 동시에 제거된 객체(제네릭으로 선언 되어진 객체) return
+    * stack의 size 메소드를 통해 순회 가능
+    * */
     public String solution(String str) {
         String answer = "";
         Stack<Character> stack = new Stack<>();
@@ -10,6 +14,7 @@ public class Practice0502 {
                while (stack.pop() != '(');
            } else stack.push(x);
         }
+        for (int i = 0; i < stack.size(); i++) answer += stack.get(i);
         return answer;
     }
 
