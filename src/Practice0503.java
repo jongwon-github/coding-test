a57f4826857134bb466a665becadf36ada7eed77
jwlee-9939-study <jwlee9939@gmail.com>
2022-05-25 06:29:41 +0900

    Stack, Queue(자료구조) - 크레인 인형뽑기 완료

--- src/Practice0503.java
+++ src/Practice0503.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Practice0503 {
-    public int solution(int n, int[][] arr1, int[] arr2) {
+    /* stack의 최 상위 값을 stack.get(stack.size() - 1) 이렇게도 가져올 수 있지만 'peek' 메소드를 통해서도 가져올 수 있음 */
+    public int solution(int[][] board, int[] moves) {
         int answer = 0;
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (arr1[j][arr2[i] - 1] != 0) {
-                    if (stack.get(stack.size() - 1) == arr1[j][arr2[i] - 1]) {
+        for (int pos : moves) {
+            for (int i = 0; i < board.length; i++) {
+                int tmp = board[i][pos - 1];
+                if (tmp != 0) {
+                    if (!stack.isEmpty() && stack.get(stack.size() - 1) == tmp) {
                         stack.pop();
-                    } else {
-                        stack.push(arr1[j][arr2[i] - 1]);
-                    }
-                    arr1[j][arr2[i] - 1] = 0;
+                        answer += 2;
+                    } else stack.push(tmp);
+                    board[i][pos - 1] = 0;
+                    break;
                 }
             }
         }
@@ -24,11 +26,15 @@ public class Practice0503 {
         Practice0503 T = new Practice0503();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        int[] arr = new int[n];
+        int[][] board = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-
+                board[i][j] = kb.nextInt();
             }
         }
+        int m = kb.nextInt();
+        int[] moves = new int[m];
+        for (int i = 0; i < m; i++) moves[i] = kb.nextInt();
+        System.out.println(T.solution(board, moves));
     }
 }
