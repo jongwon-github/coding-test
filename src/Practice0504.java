e500440a7bc89a0a5a9d499d7443cc66e06d0764
jwlee-9939-study <jwlee9939@gmail.com>
2022-05-28 17:07:27 +0900

    Stack, Queue(자료구조) - 쇠막대기

--- src/Practice0504.java
+++ src/Practice0504.java
@@ -5,32 +5,17 @@ public class Practice0504 {
     public int solution(String str) {
         int answer = 0;
         Stack<Integer> stack = new Stack<>();
-        for (char ch : str.toCharArray()) {
-            int lt = 0;
-            int rt = 0;
-            if (!stack.isEmpty()) {
-                System.out.println("여기를 들어오나요?");
-                lt = stack.pop();
-                rt = stack.pop();
+        for (char x : str.toCharArray()) {
+            /* char는 숫자를 0 -> 48, 1 -> 49 ... 으로 인식하기 때문에 아래와 같이 48을 빼줌으로써 실제 숫자값으로 인식되게 함 */
+            if (Character.isDigit(x)) stack.push(x - 48);
+            else {
+                int rt = stack.pop();
+                int lt = stack.pop();
+                if (x == '+') stack.push(rt + lt);
+                else if (x == '-') stack.push(rt - lt);
+                else if (x == '*') stack.push(lt * rt);
+                else if (x == '/') stack.push(lt / rt);
             }
-            switch (ch) {
-                case '+':
-                    stack.push(lt + rt);
-                    break;
-                case '-':
-                    stack.push(lt - rt);
-                    break;
-                case '*':
-                    stack.push(lt * rt);
-                    break;
-                case '/':
-                    stack.push(lt / rt);
-                    break;
-                default:
-                    stack.push(Character.getNumericValue(ch));
-                    break;
-            }
-
         }
         answer = stack.get(0);
         return answer;
