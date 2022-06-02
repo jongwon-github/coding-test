af99d50dcb0fe02eaf00371ccd4ad2d8b0455ebb
jwlee-9939-study <jwlee9939@gmail.com>
2022-06-02 22:49:24 +0900

    Sorting and Searching(정렬, 이분검색과 결정알고리즘) - 선택정렬

---
+++ src/Practice0601.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Practice0601 {
+    public int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n; i++) {
+            int idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[idx] > arr[j]) idx = j;
+            }
+            int tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Practice0601 T = new Practice0601();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+        for (int x : T.solution(n, arr)) System.out.print(x + " ");
+    }
+}
