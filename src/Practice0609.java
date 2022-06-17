3f33fdb6d2bb3b4d519f21ad899c9eb3c2cc38d3
jwlee-9939-study <jwlee9939@gmail.com>
2022-06-17 21:12:41 +0900

    Sorting and Searching(정렬, 이분검색과 결정알고리즘) - 뮤직비디오(결정알고리즘) 완료

--- src/Practice0609.java
+++ src/Practice0609.java
@@ -2,28 +2,39 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Practice0609 {
+    public int count(int[] arr, int capacity) {
+        int cnt = 1, sum = 0;
+        /*for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum > capacity) {
+                cnt++;
+                sum = arr[i];
+            }
+        }*/
+        for (int x : arr) {
+            if (sum + x > capacity) {
+                cnt++;
+                sum = x;
+            } else sum += x;
+        }
+        return cnt;
+    }
+
     public int solution(int n, int m, int[] arr) {
         int answer = 0, lt = 0, rt = 0;
         // 1. 노래를 담을 수 있는 cd의 최소용량과 최대용량을 설정
         // 2. 최소용량은 배열에 담기 마직막 노래의 용량(노래의 용량값이 오름차순으로 정리되어 있기 때문)
-        lt = arr[n - 1];
-        for (int i = 0; i < n; i++) {
-            rt += arr[i];
-        }
+        // 3. 최대용량은 배열에 담긴 모든 노래 용량의 합
+        lt = Arrays.stream(arr).max().getAsInt();
+        rt = Arrays.stream(arr).sum();
         while (lt <= rt) {
-            int hap = 0;
-            int cnt = 0;
             int mid = (lt + rt) / 2;
-            for (int i = 0; i < n - 1; i++) {
-                hap += arr[i];
-                if (hap > mid) {
-                    cnt++;
-                    hap = arr[i];
-                }  
-            }
-            if (cnt <= 3) rt = mid;
-            else lt = mid;
+            if (count(arr, mid) <= m) {
+                rt = mid - 1;
+                answer = mid;
+            } else lt = mid + 1;
         }
+        return answer;
     }
 
     public static void main(String[] args) {
