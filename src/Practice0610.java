3e5d6a7b136b89ea4b7a124d45b10a8411962a0b
jwlee-9939-study <jwlee9939@gmail.com>
2022-06-19 18:13:50 +0900

    Sorting and Searching(정렬, 이분검색과 결정알고리즘) - 마구간 정하기(결정알고리즘) 완료

--- src/Practice0610.java
+++ src/Practice0610.java
@@ -2,11 +2,23 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Practice0610 {
-    public int solution(int n, int m, int[] arr) {
+    public int count(int[] arr, int dist) {
+        int cnt = 1;
+        int ep = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - ep >= dist) {
+                cnt++;
+                ep = arr[i];
+            }
+        }
+        return cnt;
+    }
+
+    public int solution(int n, int c, int[] arr) {
         int answer = 0;
         Arrays.sort(arr);
         // 두 말의 거리값 중 최소값 -> 이건 최대값과 다르게 arr[0]으로 세팅하면 안됨
-        // ex) 5, 8, 9, 10, 11 일 경우 거리의 최대값은 8 - 5 = 3 인데 arr[0]으로 세팅되면 안됨
+        // ex) 5, 8, 9, 10, 11 일 경우 거리의 최대값은 8 - 5 = 3 인데 arr[0]으로 세팅되면 최소 거리값이 5가 되기 때문에 올바른 답을 구할 수 없음
         int lt = arr[1] - arr[0];
         // 두 말의 거리값 중 최대값 -> (arr[n - 1] - arr[0]) 값이 정확할 거 같은데 어차피 이분검색으로 값을 줄여 나날거라 마구간 위치 정보 중 제일 큰 값으로 세팅
         int rt = arr[n - 1];
@@ -14,21 +26,12 @@ public class Practice0610 {
             // 이전 마구간의 위치 정보
             int ep = arr[0];
             int mid = (lt + rt) / 2;
-            int cnt = 1;
-            for (int i = 0; i < n; i++) {
-                if (arr[i] - ep >= mid) {
-                    ep = arr[i];
-                    cnt++;
-                }
-            }
-            if (cnt < m) {
-                rt = mid - 1;
-            } else {
+            if (count(arr, mid) >= c) {
                 answer = mid;
                 lt = mid + 1;
-            }
+
+            } else rt = mid - 1;
         }
-        System.out.println("anser : " + answer);
         return answer;
     }
 
@@ -36,11 +39,11 @@ public class Practice0610 {
         Practice0610 T = new Practice0610();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        int m = kb.nextInt();
+        int c = kb.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = kb.nextInt();
         }
-        System.out.println(T.solution(n, m, arr));
+        System.out.println(T.solution(n, c, arr));
     }
 }
