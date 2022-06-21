7a1ffff594a7a1797312c0e141692f7f80c44b26
HKMC\H2004012 <jwlee9939@gmail.com>
2022-06-21 14:11:54 +0900

    Recursive, Tree, Graph(DFS, BFS 기초) - 이진수 출력(재귀)

---
+++ src/Practice0702.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Practice0702 {
+    public void DFS(int n) {
+       if (n == 0) return;
+       else {
+           DFS(n / 2);
+           System.out.print(n % 2);
+       }
+    }
+
+    public static void main(String[] args){
+        Practice0702 T = new Practice0702();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        T.DFS(n);
+    }
+}
