3183ff6a3d855f248841eea8e825d0260419db22
HKMC\H2004012 <jwlee9939@gmail.com>
2022-06-23 17:50:56 +0900

    Recursive, Tree, Graph(DFS, BFS 기초) - 피보나치 수열(메모이제이션) 각 단계별 값 저장 및 출력

--- src/Practice0704.java
+++ src/Practice0704.java
@@ -1,12 +1,17 @@
 public class Practice0704 {
+    static int[] fibo;
     public int DFS(int n) {
-        if (n == 1 || n == 2) return 1;
-        else return DFS(n - 1) + DFS(n - 2);
+        if (n == 1 || n == 2) return fibo[n] = 1;
+        else return fibo[n] = DFS(n - 1) + DFS(n - 2);
     }
 
     public static void main(String[] args) {
         Practice0704 T = new Practice0704();
         int n = 5;
-        System.out.println(T.DFS(n));
+        fibo = new int[n + 1];
+        T.DFS(n);
+        for (int i = 1; i <= n; i++) {
+            System.out.print(fibo[i] + " ");
+        }
     }
 }
