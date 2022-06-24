0a3ef0e202841b1c6f2f2601cce2cc8a155bd802
HKMC\H2004012 <jwlee9939@gmail.com>
2022-06-24 10:23:00 +0900

    Recursive, Tree, Graph(DFS, BFS 기초) - 피보니치 재귀(메모이제이션) 완료

--- src/Practice0704.java
+++ src/Practice0704.java
@@ -1,13 +1,15 @@
 public class Practice0704 {
     static int[] fibo;
+
     public int DFS(int n) {
+        if (fibo[n] != 0) return fibo[n];
         if (n == 1 || n == 2) return fibo[n] = 1;
         else return fibo[n] = DFS(n - 1) + DFS(n - 2);
     }
 
     public static void main(String[] args) {
         Practice0704 T = new Practice0704();
-        int n = 5;
+        int n = 100;
         fibo = new int[n + 1];
         T.DFS(n);
         for (int i = 1; i <= n; i++) {
