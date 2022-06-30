824340ad544db698060dddc2e1dca362b95f08cf
HKMC\H2004012 <jwlee9939@gmail.com>
2022-06-30 17:04:07 +0900

    Recursive, Tree, Graph(DFS, BFS 기초) - 부분집합 구하기(DFS)

--- src/Practice0706.java
+++ src/Practice0706.java
@@ -1,9 +1,16 @@
 public class Practice0706 {
     static int n;
     static int[] ch;
+    
     public void DFS(int L) {
         if (L == n + 1) {
-
+           String tmp = "";
+           for (int i = 1; i <= n; i++) {
+               if (ch[i] == 1) tmp += (i + " ");
+           }
+           if (tmp.length() > 0) {
+               System.out.println(tmp);
+           }
         } else {
             ch[L] = 1;
             DFS(L + 1);
