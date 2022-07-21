5828397401d8f26848e380b7f956e93fc9c9143a
jwlee-9939-study <jwlee9939@gmail.com>
2022-07-22 06:44:13 +0900

    DFS, BFS 활용 - 바둑이 승차

---
+++ src/Practice0802.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Practice0802 {
+    static int answer = Integer.MIN_VALUE, c, n;
+
+    public void DFS(int L, int sum, int[] arr) {
+        if (sum > c) return;
+        if (L == n) {
+            answer = Math.max(answer, sum);
+        } else {
+            DFS(L + 1, sum + arr[L], arr);
+            DFS(L + 1, sum, arr);
+        }
+    }
+
+    public static void main(String[] args) {
+        Practice0802 T = new Practice0802();
+        Scanner kb = new Scanner(System.in);
+        c = kb.nextInt();
+        n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        T.DFS(0, 0, arr);
+        System.out.println(answer);
+    }
+}
