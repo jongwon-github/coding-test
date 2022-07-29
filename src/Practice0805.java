f58a74367da7fd1197fb6a259e28219884c7fb17
HKMC\H2004012 <jwlee9939@gmail.com>
2022-07-29 10:23:21 +0900

    9fe5851: DFS, BFS 활용 - 동전교환 완료

--- src/Practice0805.java
+++ src/Practice0805.java
@@ -1,17 +1,18 @@
+import java.util.*;
+
 public class Practice0805 {
     static int n, m, answer = Integer.MAX_VALUE;
 
     public void DFS(int L, int sum, int[] arr) {
         if (sum > m) return;
-        else {
-            if (sum == m) {
-                answer = Math.max(answer, L);
-                return answer;
-            }
+        if (L >= answer) return;
+        if (sum == m) {
+            answer = Math.min(answer, L);
+        } else {
             for (int i = 0; i < n; i++) {
-                DFS(++L, sum + arr[i], arr);
+                DFS(L + 1, sum + arr[i], arr);
             }
-        } 
+        }
     }
 
     public static void main(String[] args) {
@@ -22,5 +23,6 @@ public class Practice0805 {
         for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
         m = kb.nextInt();
         T.DFS(0, 0, arr);
+        System.out.println(answer);
     }
 }
