a8863e928f23325ea672a6025456da8d570b9b96
jwlee-9939-study <jwlee9939@gmail.com>
2022-08-01 22:45:58 +0900

    DFS, BFS 활용 - 순열 구하기 완료

--- src/Practice0806.java
+++ src/Practice0806.java
@@ -1,20 +1,18 @@
 import java.util.*;
 
 public class Practice0806 {
-    static int[] pm, ch, test;
+    static int[] pm, ch, arr;
     static int n, m;
 
     public void DFS(int L) {
         if (L == m) {
-            if (test.length == 2) {
-                for (int x : test) System.out.print(x + " ");
-                System.out.println();
-            }
+            for (int x : pm) System.out.print(x + " ");
+            System.out.println();
         } else {
             for (int i = 0; i < n; i++) {
                 if (ch[i] == 0) {
                     ch[i] = 1;
-                    test[L] = pm[i];
+                    pm[L] = arr[i];
                     DFS(L + 1);
                     ch[i] = 0;
                 }
@@ -27,10 +25,10 @@ public class Practice0806 {
         Scanner kb = new Scanner(System.in);
         n = kb.nextInt();
         m = kb.nextInt();
-        pm = new int[n];
-        for (int i = 0; i < n; i++) pm[i] = kb.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
         ch = new int[n];
-        test = new int[m];
+        pm = new int[m];
         T.DFS(0);
     }
 }
