503e6686dd60c921c6fb0e0677a4503912e2c419
jwlee-9939-study <58896783+jwlee-9939-study@users.noreply.github.com>
2022-08-03 17:16:33 +0900

    Create Practice0807.java
    DFS, BFS 활용 - 조합수(메모이제이션) 진행 중

---
+++ src/Practice0807.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Practice0807 {
+    int[][] dy = new int[35][35];
+
+    public int DFS(int n, int r) {
+        if (n == r || r == 0) return 1;
+        else return DFS(n - 1, r) + DFS(n - 1, r - 1);
+    }
+
+    public static void main(String[] args) {
+        Practice0807 T = new Practice0807();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int r = kb.nextInt();
+        System.out.println(T.DFS(n, r));
+    }
+}
