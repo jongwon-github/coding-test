8a010463c989d2a68aa2be6eeb3a703d7b85ab3c
jwlee-9939-study <jwlee9939@gmail.com>
2022-08-13 23:40:36 +0900

    DFS, BFS 활용 - 조합 구하기 진행 중

---
+++ src/Practice0809.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Practice0809 {
+    static int[] combi;
+    static int n, m;
+
+    public void DFS(int L, int s) {
+        if (L == m) {
+            for (int x : combi) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        } else {
+            for (; s <= m; s++) {
+                combi[L] = s;
+                DFS(L + 1, s + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Practice0809 T = new Practice0809();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        combi = new int[m];
+        T.DFS(0, 1);
+    }
+}
