cb6aa093b57af5e3a33ff1a987274fdb51488872
jwlee-9939-study <jwlee9939@gmail.com>
2023-01-15 22:32:28 +0900

    dynamic programming(동적계획법) - 최대부분증가수열(LIS), 주석 추가

--- src/Practice1003.java
+++ src/Practice1003.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Practice1003 {
+    // 해당 배열값에는 동일한 인덱스의 arr 배열값을 끝으로하는 부분증가수열값이 들어감
     static int[] dy;
 
     public int solution(int[] arr) {
@@ -10,6 +11,7 @@ public class Practice1003 {
         for (int i = 1; i < arr.length; i++) {
             int max = 0;
             for (int j = i - 1; j >= 0; j--) {
+                // 현재 배열(arr[i])의 값이 이전 배열(arr[j])값보다 작다면 로직 실행 불필요 : 부분증가수열은 arr[i]값이 최대인 부분증가수열값을 구해야 하기 때문에 자신보다 큰값은 제외를 시켜야 함
                 if (arr[j] < arr[i] && dy[j] > max) max = dy[j];
             }
             dy[i] = max + 1;
