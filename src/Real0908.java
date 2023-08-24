1d6138a59eb2082ac4b0a366cdddd82a8e472ec4
jwlee-9939-study <jwlee9939@gmail.com>
2023-08-24 23:52:49 +0900

    파일 삭제

--- src/Real0908.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Real0908 {
-    public static void main(String[] args) {
-        Real0908 T = new Real0908();
-        Scanner kb = new Scanner(System.in);
-    }
-}
