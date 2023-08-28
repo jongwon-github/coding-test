d668e71cf3ad755f98ed2325ea4dba401cdf088b
jongwon <58896783+jongwon-github@users.noreply.github.com>
2023-08-28 17:19:33 +0900

    IP 주소 완료

--- src/CdTest0605.java
+++ src/CdTest0605.java
@@ -1,55 +1,69 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class CdTest0605 {
-    ArrayList<String> arList = new ArrayList<>();
-
-    public boolean check(String component) {
-        boolean flag = true;
-        if (component.length() > 2) {
-            if (component.startsWith("0") || Integer.parseInt(component) > 255) flag = false;
-        }
-        return flag;
-    }
+    LinkedList<String> tmp;
+    ArrayList<String> res;
 
     public void DFS(int L, int start, String s) {
-        if (start >= s.length() - 1) return;
-        if (L == 4) {
-            String tmp = "";
-            for (String str : arList) {
-                tmp += str;
-            }
-            if (tmp.length() == s.length()) {
-                System.out.println(tmp);
-                System.out.println();
-            }
-            arList = new ArrayList<>();
+        if (L == 4 && start == s.length()) {
+            String Ts = "";
+            for(String x : tmp) Ts += x + ".";
+            res.add(Ts.substring(0, Ts.length()-1));
         } else {
-            for (int i = start; i < s.length() - 2; i++) {
-                String component = s.substring(start, i + 1);
-                if (check(component)) {
-                    arList.add(component);
-                    DFS(L + 1, i + 1, s);
-                }
+            for (int i = start; i < s.length(); i++) {
+                // '0'으로 시작하면서 길이가 2 이상인 경우 return
+                if (s.charAt(start) == '0' && i > start) return;
+                String num = s.substring(start, i + 1);
+                // num 이 255 보다 큰 경우 return
+                if (Integer.parseInt(num) > 255) return;
+                tmp.add(num);
+                DFS(L + 1, i + 1, s);
+                // 호출한 재귀함수가 종료 되었을 때, tmp 마지막 요소 제거
+                tmp.remove(L);
             }
         }
     }
 
+    /**
+     * 풀이에 나와있는 답
+     * int L 변수는 사용하지 않음
+     */
+//    public void DFS(int start, String s){
+//        if(tmp.size() == 4 && start == s.length()){
+//            String Ts = "";
+//            for(String x : tmp) Ts += x + ".";
+//            res.add(Ts.substring(0, Ts.length()-1));
+//        }
+//        else{
+//            for(int i = start; i < s.length(); i++){
+//                if(s.charAt(start) == '0' && i > start) return;
+//                String num = s.substring(start, i + 1);
+//                if(Integer.parseInt(num) > 255) return;
+//                tmp.add(num);
+//                DFS(i + 1, s);
+//                tmp.pollLast();
+//            }
+//        }
+//    }
+
     public String[] solution(String s) {
-        String[] answer = {};
+        tmp = new LinkedList<>();
+        res = new ArrayList<>();
         DFS(0, 0, s);
+        String[] answer = new String[res.size()];
+        for(int i = 0; i < res.size(); i++) answer[i] = res.get(i);
         return answer;
     }
 
     public static void main(String[] args) {
         CdTest0605 T = new CdTest0605();
         System.out.println(Arrays.toString(T.solution("2025505")));
-//        System.out.println(Arrays.toString(T.solution("0000")));
-//        System.out.println(Arrays.toString(T.solution("255003")));
-//        System.out.println(Arrays.toString(T.solution("155032012")));
-//        System.out.println(Arrays.toString(T.solution("02325123")));
-//        System.out.println(Arrays.toString(T.solution("121431211")));
-//        String test = "abc";
-//        System.out.println(test.substring(2, 0));
+        System.out.println(Arrays.toString(T.solution("0000")));
+        System.out.println(Arrays.toString(T.solution("255003")));
+        System.out.println(Arrays.toString(T.solution("155032012")));
+        System.out.println(Arrays.toString(T.solution("02325123")));
+        System.out.println(Arrays.toString(T.solution("121431211")));
     }
 }
